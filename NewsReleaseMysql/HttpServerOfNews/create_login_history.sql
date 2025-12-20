bfa8c6832ca5ac080d1f6b5275573746260f8d08
waik <32024755@qq.com>
2025-12-21 00:24:52 +0800

    commit

--- NewsReleaseMysql/HttpServerOfNews/create_login_history.sql
+++
@@ -1,9 +0,0 @@
-USE newsrelease;
-
-CREATE TABLE `login_history` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `login_time` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
